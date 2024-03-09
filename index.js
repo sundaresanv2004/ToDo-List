8f100dc63e693b1689ced432b8d77b70481b9002
Sundaresan <sundaresanv2004@gmail.com>
2024-03-09 20:08:52 +0530

    0.03

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const activeTasksCountElement = document.getElementById("total_task");
+const activeTasksCountElement = document.getElementById("total_task");
 
-    fetch("assets/data.json")
-        .then(response => response.json())
-        .then(data => {
-            activeTasksCountElement.textContent = data.items.filter(item => !item.completed).length;
-        })
+document.addEventListener("DOMContentLoaded", function () {
+    const taskList = document.getElementById("todoList");
+    const addTaskButton = document.getElementById("saveTaskButton");
 
-        .catch(error => console.error("Error fetching JSON:", error));
+    addTaskButton.addEventListener("click", function () {
+        const newTaskInput = document.getElementById("newTask");
+        const taskText = newTaskInput.value.trim();
+
+        if (taskText !== "") {
+            const taskItem = document.createElement("div");
+            taskItem.className = "task";
+            taskItem.innerHTML = `
+                    <div class="todo-item">
+                           <div class="checkbox-and-title">
+                               <input type="checkbox" id="${taskText}">
+                               <label for="${taskText}">${taskText}</label>
+                           </div>
+                           <span class="material-icons edit-icon">edit</span>
+                           <span class="material-icons delete-icon" onclick="deleteTask(this)">delete</span>
+                    </div>
+                `;
+            taskList.appendChild(taskItem);
+            newTaskInput.value = "";
+            var divElements = taskList.getElementsByClassName('task');
+            activeTasksCountElement.textContent = divElements.length;
+        }
+    });
 });
+
+function deleteTask(button) {
+    const taskItem = button.parentNode;
+    taskItem.parentNode.removeChild(taskItem);
+
+    var currentText = activeTasksCountElement.textContent
+    activeTasksCountElement.textContent = currentText - 1
+}
 
 function toggleDialog() {
     var dialogContainer = document.getElementById('dialog-container');
