ae436f30099dcb731792590a55ac95a5c7a29997
Sundaresan <sundaresanv2004@gmail.com>
2024-03-10 12:48:55 +0530

    0.04

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const activeTasksCountElement = document.getElementById("total_task");
-
 document.addEventListener("DOMContentLoaded", function () {
     const taskList = document.getElementById("todoList");
     const addTaskButton = document.getElementById("saveTaskButton");
+    const activeTasksCountElement = document.getElementById("total_task");
 
     addTaskButton.addEventListener("click", function () {
         const newTaskInput = document.getElementById("newTask");
@@ -13,28 +12,30 @@ document.addEventListener("DOMContentLoaded", function () {
             taskItem.className = "task";
             taskItem.innerHTML = `
                     <div class="todo-item">
-                           <div class="checkbox-and-title">
-                               <input type="checkbox" id="${taskText}">
-                               <label for="${taskText}">${taskText}</label>
-                           </div>
-                           <span class="material-icons edit-icon">edit</span>
-                           <span class="material-icons delete-icon" onclick="deleteTask(this)">delete</span>
+                        <div class="checkbox-and-title">
+                            <input type="checkbox" id="${taskText}">
+                            <label for="${taskText}">${taskText}</label>
+                        </div>
+                        <span class="material-icons edit-icon">edit</span>
+                        <span class="material-icons delete-icon" onclick="deleteTask(this)">delete</span>
                     </div>
                 `;
             taskList.appendChild(taskItem);
             newTaskInput.value = "";
+
             var divElements = taskList.getElementsByClassName('task');
             activeTasksCountElement.textContent = divElements.length;
         }
     });
 });
 
+
 function deleteTask(button) {
     const taskItem = button.parentNode;
     taskItem.parentNode.removeChild(taskItem);
 
-    var currentText = activeTasksCountElement.textContent
-    activeTasksCountElement.textContent = currentText - 1
+    var currentText = activeTasksCountElement.textContent;
+    activeTasksCountElement.textContent = parseInt(currentText) - 1;
 }
 
 function toggleDialog() {
